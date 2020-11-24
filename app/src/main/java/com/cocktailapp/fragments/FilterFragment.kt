1b9f6d80c1bdddbb8a237ed7838f3b5d4b753dcc
DaniilChumachenko <metalmen.men@gmail.com>
2020-11-24 02:16:29 +0200

    Master. Add bad filtration by categories.

--- app/src/main/java/com/cocktailapp/fragments/FilterFragment.kt
+++ app/src/main/java/com/cocktailapp/fragments/FilterFragment.kt
@@ -4,22 +4,58 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.cocktailapp.R
+import com.cocktailapp.adapter.СategoryAdapter
+import com.cocktailapp.api.CommonRetrofit
+import com.cocktailapp.api.RequestRetrofit
+import com.cocktailapp.model.Drink
+import com.cocktailapp.model.DrinksArray
+import kotlinx.android.synthetic.main.fragment_filter.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FilterFragment : Fragment() {
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
-    }
+    lateinit var requestRetrofit: RequestRetrofit
+    lateinit var categoryAdapter: СategoryAdapter
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_filter, container, false)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        requestRetrofit = CommonRetrofit.requestRetrofitService
+        getCategoryies()
+        val bundle = Bundle()
+        bt_apply_filter.setOnClickListener {
+
+            bundle.putStringArrayList("categories", categoryAdapter.selectedCategories)
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment,bundle )
+        }
+    }
+    private fun getCategoryies() {
+        requestRetrofit.getAllCategories("list").enqueue(object : Callback<DrinksArray> {
+            override fun onFailure(call: Call<DrinksArray>, t: Throwable) {
+            }
+
+            override fun onResponse(call: Call<DrinksArray>, response: Response<DrinksArray>) {
+                rv_filter_fragment.setHasFixedSize(true)
+                rv_filter_fragment.layoutManager = LinearLayoutManager(context)
+                categoryAdapter = СategoryAdapter(response.body()?.drinks as List<Drink>)
+                categoryAdapter.notifyDataSetChanged()
+                rv_filter_fragment.adapter = categoryAdapter
+            }
+        })
     }
 }
--- app/src/main/java/com/cocktailapp/viewmodels/OrdinaryDrinksViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.cocktailapp.viewmodels
-
-import androidx.lifecycle.ViewModel
-
-class OrdinaryDrinksViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
